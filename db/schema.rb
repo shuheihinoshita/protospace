967b76e0ee8f0d5dba794930cf4525962d3b768f
日下修平 <ShuheiHinoshita@ShuheiHinoshita.local>
2016-02-03 10:36:20 +0900

    create image model and columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203012507) do
+ActiveRecord::Schema.define(version: 20160203013222) do
+
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "prototype_id"
+    t.integer  "status"
+    t.string   "content"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "prototypes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
