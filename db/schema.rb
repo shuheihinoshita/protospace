b2341af0cb26ac85316bd9ff7c7f6923733c34bb
日下修平 <ShuheiHinoshita@ShuheiHinoshita.local>
2016-02-03 10:29:34 +0900

    create prototype model and columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203010731) do
+ActiveRecord::Schema.define(version: 20160203012507) do
+
+  create_table "prototypes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.text     "title",      limit: 65535
+    t.text     "catch_copy", limit: 65535
+    t.text     "concept",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
